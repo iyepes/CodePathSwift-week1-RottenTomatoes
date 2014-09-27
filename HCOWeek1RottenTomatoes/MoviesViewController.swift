e8b9594990f2349871fc3aea834997292191bd11
Isabel Yepes <isabel.yepes@silveryfrog.com>
2014-09-26 21:56:01 -0700

    - Basic movies view app.

--- HCOWeek1RottenTomatoes/MoviesViewController.swift
+++ HCOWeek1RottenTomatoes/MoviesViewController.swift
@@ -11,12 +11,25 @@ import UIKit
 class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var tableView: UITableView!
+    var movies: [NSDictionary] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         tableView.delegate = self
         tableView.dataSource = self
+        
+        var url = "http://api.rottentomatoes.com/api/public/v1.0/lists/movies/box_office.json?apikey=zs244f7v8xdzm6unwf8m97xy&limit=20&country=us"
+        
+        var request = NSURLRequest(URL: NSURL(string: url))
+        
+        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue()) { (response: NSURLResponse!,data: NSData!, error: NSError!) -> Void in
+        var object = NSJSONSerialization.JSONObjectWithData(data, options: nil, error: nil) as NSDictionary
+        self.movies = object["movies"] as [NSDictionary]
+        //println("object \(object)")
+        self.tableView.reloadData()
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,12 +38,28 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return movies.count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        var cell = UITableViewCell()
-        cell.textLabel!.text = "Hello I'm at row: \(indexPath.row), section: \(indexPath.section)"
+        
+        var cell = tableView.dequeueReusableCellWithIdentifier("MovieCell") as MovieCell
+        
+        var movie = movies[indexPath.row]
+        
+        cell.titleLabel.text = "Title"
+        cell.synopsisLabel.text = "Synopsis"
+        
+        cell.titleLabel.text = movie["title"] as? String
+        cell.synopsisLabel.text = movie["synopsis"] as? String
+        
+        var posters = movie["posters"] as NSDictionary
+        var posterURL = posters["thumbnail"] as String
+        
+        cell.posterView.setImageWithURL(NSURL(string: posterURL))
+        
+        //var cell = UITableViewCell()
+        //cell.textLabel!.text = "Hello I'm at row: \(indexPath.row), section: \(indexPath.section)"
         return cell
     }
     
